06c3a0f9267ca3762a678eb372bfc291d616d860
Femi <alloolorunfemi@gmail.com>
2023-10-03 21:19:27 +0100

    changed address

--- src/App.js
+++ src/App.js
@@ -1,97 +1,159 @@
-
-import { BrowserRouter,Routes,NavLink, Route, useLocation } from "react-router-dom";
+import {
+  BrowserRouter,
+  Routes,
+  NavLink,
+  Route,
+  useLocation,
+} from "react-router-dom";
 import Home from "./components/home";
 import About from "./components/cryptoservices";
 import Blog from "./components/blog";
 import Services from "./components/services";
 import Contact from "./components/contact";
-import Icon from "./assets/icon.svg"
-import { FaBars, FaEnvelope, FaHamburger, FaInstagram, FaMapMarkerAlt, FaPhone, FaPhoneAlt, FaTelegram, FaTimes, FaTwitter, FaWhatsapp } from "react-icons/fa";
-import {HiMenuAlt3} from "react-icons/hi"
+import Icon from "./assets/icon.svg";
+import {
+  FaBars,
+  FaEnvelope,
+  FaHamburger,
+  FaInstagram,
+  FaMapMarkerAlt,
+  FaPhone,
+  FaPhoneAlt,
+  FaTelegram,
+  FaTimes,
+  FaTwitter,
+  FaWhatsapp,
+} from "react-icons/fa";
+import { HiMenuAlt3 } from "react-icons/hi";
 import Cryptoservices from "./components/cryptoservices";
 import { useState } from "react";
 import Preload from "./components/preload";
 import { useEffect } from "react";
 
 const App = () => {
-  const [showNav, setShowNav] = useState(false)
-const nav = document.getElementById('nav')
-  const handleNav = ()=>{
-    setShowNav(prevstate => !prevstate);
-  }
+  const [showNav, setShowNav] = useState(false);
+  const nav = document.getElementById("nav");
+  const handleNav = () => {
+    setShowNav((prevstate) => !prevstate);
+  };
 
-  const handleLinkClick = ()=>{
-    setShowNav(false)
-  }
- const [loading, setLoading] = useState(true)
- useEffect(()=>{
- setTimeout(() => {
-    setLoading(false);
-  }, 500);
-
- }, [])
+  const handleLinkClick = () => {
+    setShowNav(false);
+  };
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 500);
+  }, []);
 
   return (
     <BrowserRouter>
-    {loading ? 
-    (<Preload/>)
-    :
-    <div>
+      {loading ? (
+        <Preload />
+      ) : (
+        <div>
           <header>
-                <div className="container headersect">
-                    <div className="logo">
-                        <NavLink to="/">
-                            <img src={Icon} alt="" />
-                            <h1>CYBERKEELV</h1>
-                        </NavLink>
-                    </div>
-                    <span  className="ham">
-                      {showNav ? <FaTimes className="cancel" onClick={handleNav} color="white" size={32}/> : <FaBars onClick={handleNav} color="white" size={32}/>}
-                    </span>
-                    <nav id='nav' className={`${showNav ? 'slideright' : ''}`}>
-                        <ul>
-                            <li><NavLink onClick={handleLinkClick} to="/">Home</NavLink></li>
-                            <li><a onClick={handleLinkClick} href="#about"> About us</a></li>
-                            <li><NavLink onClick={handleLinkClick} to="/services">Services</NavLink></li>
-                            <li><NavLink onClick={handleLinkClick} to="/blogs">News</NavLink></li>
-                            <li><button><NavLink onClick={handleLinkClick} to="/contact">Contact</NavLink></button></li>
-                        </ul>
-                    </nav>
-                </div>
+            <div className="container headersect">
+              <div className="logo">
+                <NavLink to="/">
+                  <img src={Icon} alt="" />
+                  <h1>CYBERKEELV</h1>
+                </NavLink>
+              </div>
+              <span className="ham">
+                {showNav ? (
+                  <FaTimes
+                    className="cancel"
+                    onClick={handleNav}
+                    color="white"
+                    size={32}
+                  />
+                ) : (
+                  <FaBars onClick={handleNav} color="white" size={32} />
+                )}
+              </span>
+              <nav id="nav" className={`${showNav ? "slideright" : ""}`}>
+                <ul>
+                  <li>
+                    <NavLink onClick={handleLinkClick} to="/">
+                      Home
+                    </NavLink>
+                  </li>
+                  <li>
+                    <a onClick={handleLinkClick} href="#about">
+                      {" "}
+                      About us
+                    </a>
+                  </li>
+                  <li>
+                    <NavLink onClick={handleLinkClick} to="/services">
+                      Services
+                    </NavLink>
+                  </li>
+                  <li>
+                    <NavLink onClick={handleLinkClick} to="/blogs">
+                      News
+                    </NavLink>
+                  </li>
+                  <li>
+                    <button>
+                      <NavLink onClick={handleLinkClick} to="/contact">
+                        Contact
+                      </NavLink>
+                    </button>
+                  </li>
+                </ul>
+              </nav>
+            </div>
           </header>
           <Routes>
-          <Route path='/' element={<Home/>}></Route>
-          <Route path="/crypto-services" element={<Cryptoservices/>}></Route>
-          <Route path="/blogs" element={<Blog/>}></Route>
-          <Route path="/services" element={<Services/>}></Route>
-          <Route path="/contact" element={<Contact/>}></Route>
-        </Routes>
-        <footer id="about">
-                <div className="container footcontainer">
-                    <div className="socials">
-                        <NavLink to="https://api.whatsapp.com/send?phone=8562097916145&text=%20"><span className="iconsocial"><FaWhatsapp  size={32} color="white"/></span></NavLink>
-                        <NavLink to="https://t.me/+37128785330"><span className="iconsocial"><FaTelegram  color="white" size={32}/></span></NavLink>
-                        <NavLink to="https://rb.gy/2xptg"><span className="iconsocial"><FaInstagram  size={32} color="white"/></span></NavLink>
-                        {/* <NavLink to="#"><span className="iconsocial"><FaTwitter  size={32} color="white"/></span></NavLink> */}
-                    </div>
-                    <cite>“The one who owns the information, owns the world!”</cite>
-                   <NavLink className="contactMail" to="#">
-                        <FaMapMarkerAlt size={15} color="white"/>
-                        <p>Outhoomphone Ban Nalaotay, Savannakhet, Laos</p>
-                    </NavLink>
-                    <NavLink className="contactMail" to="mailto:Cyberkeelv_recovery@cyberservices.com">
-                        <FaEnvelope size={15} color="white"/>
-                        <p>Cyberkeelv_recovery@cyberservices.com</p>
-                    </NavLink>
-                    <div className="copyright">
-                        <p>Cyberkeelv_recovery© Copyright 2023. All rights reserved</p>
-                    </div>
-                </div>
-            </footer>
-    </div>
-             }  
+            <Route path="/" element={<Home />}></Route>
+            <Route path="/crypto-services" element={<Cryptoservices />}></Route>
+            <Route path="/blogs" element={<Blog />}></Route>
+            <Route path="/services" element={<Services />}></Route>
+            <Route path="/contact" element={<Contact />}></Route>
+          </Routes>
+          <footer id="about">
+            <div className="container footcontainer">
+              <div className="socials">
+                <NavLink to="https://api.whatsapp.com/send?phone=8562097916145&text=%20">
+                  <span className="iconsocial">
+                    <FaWhatsapp size={32} color="white" />
+                  </span>
+                </NavLink>
+                <NavLink to="https://t.me/+79216482743">
+                  <span className="iconsocial">
+                    <FaTelegram color="white" size={32} />
+                  </span>
+                </NavLink>
+                <NavLink to="https://rb.gy/2xptg">
+                  <span className="iconsocial">
+                    <FaInstagram size={32} color="white" />
+                  </span>
+                </NavLink>
+                {/* <NavLink to="#"><span className="iconsocial"><FaTwitter  size={32} color="white"/></span></NavLink> */}
+              </div>
+              <cite>“The one who owns the information, owns the world!”</cite>
+              <NavLink className="contactMail" to="#">
+                <FaMapMarkerAlt size={15} color="white" />
+                <p>Sytinskiy Lane, 5 с2, Moscow Russia 123104</p>
+              </NavLink>
+              <NavLink
+                className="contactMail"
+                to="mailto:Cyberkeelv_recovery@cyberservices.com"
+              >
+                <FaEnvelope size={15} color="white" />
+                <p>Cyberkeelv_recovery@cyberservices.com</p>
+              </NavLink>
+              <div className="copyright">
+                <p>Cyberkeelv_recovery© Copyright 2023. All rights reserved</p>
+              </div>
+            </div>
+          </footer>
+        </div>
+      )}
     </BrowserRouter>
-    
   );
 };
 
